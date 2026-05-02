074adcf4ef4b53558ebcf82dd0aadecb86f57c0b
saplio <262173844+saplio@users.noreply.github.com>
2026-05-01 20:17:17 -0400

    home bar and group stage start

--- src/GroupPhaseController.java
+++ src/GroupPhaseController.java
@@ -1,12 +1,45 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 /*
     Controller for group-stage.fxml
 */
 
 public class GroupPhaseController extends BaseController{
 
+    @FXML private Button group1;
+    @FXML private Button group2;
+    @FXML private Button group3;
+    @FXML private Button group4;
+    @FXML private Button group5;
+    @FXML private Button group6;
+    @FXML private Button group7;
+    @FXML private Button group8;
+    @FXML private Button group9;
+    @FXML private Button group10;
+    @FXML private Button group11;
+    @FXML private Button group12;
+
     @Override
     protected void onLoad(){
+        group1.setOnAction(e -> showGroup(1));
+        group2.setOnAction(e -> showGroup(2));
+        group3.setOnAction(e -> showGroup(3));
+        group4.setOnAction(e -> showGroup(4));
+        group5.setOnAction(e -> showGroup(5));
+        group6.setOnAction(e -> showGroup(6));
+        group7.setOnAction(e -> showGroup(7));
+        group8.setOnAction(e -> showGroup(8));
+        group9.setOnAction(e -> showGroup(9));
+        group10.setOnAction(e -> showGroup(10));
+        group11.setOnAction(e -> showGroup(11));
+        group12.setOnAction(e -> showGroup(12));
 
     }
     
+    @FXML
+    private void showGroup(int i) {
+        System.out.println(i);
+        //TODO: open single group view for corresponding group
+    }
 }
