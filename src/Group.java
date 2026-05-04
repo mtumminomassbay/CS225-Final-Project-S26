b4d6657c8408ce7fc00005588d7a4876534c2c45
jpjcode <jpjoseph8145@gmail.com>
2026-05-04 02:35:14 -0400

    Added Group and GroupResults

---
+++ src/Group.java
@@ -0,0 +1,45 @@
+import java.util.Map;
+import java.util.List;
+
+public class Group
+{
+
+    private String groupName;
+    private List<Team> teams;
+    private List<Match> matches;
+    private Map<Team, GroupResults> groupResults;
+
+    private int matchesPerTeamPair;
+    private int matchesPlayed;
+
+    public Group(String groupName, List<Team> teams, int matchesPerTeamPair) {
+        this.groupName = groupName;
+        this.teams = teams;
+        this.matchesPerTeamPair = matchesPerTeamPair;
+    }
+
+    public int getMatchesPerTeamPair() {
+        return matchesPerTeamPair;
+    }
+
+    public int getMatchesPlayed() {
+        return matchesPlayed;
+    }
+
+    public List<Match> getMatches() {
+        return matches;
+    }
+
+    public List<Team> getTeams() {
+        return teams;
+    }
+
+    public Map<Team, GroupResults> getGroupResults() {
+        return groupResults;
+    }
+
+
+    public String getGroupName() {
+        return groupName;
+    }
+}
