b8c1a98292b0855856e3aef14aa592ca55cfa4cf
sky <54379974+skyguygamer@users.noreply.github.com>
2026-05-07 11:24:52 -0400

    Revert "Andrew front end"

--- src/TeamInfoAdditionalController.java
+++ src/TeamInfoAdditionalController.java
@@ -8,20 +8,21 @@ import javafx.scene.control.TableView;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
-import javafx.scene.text.Font;
-import javafx.scene.control.Label;
 
 public class TeamInfoAdditionalController extends BaseController {
     
     //ATTRIBUTES
     @FXML
-    private Label countryNameAdditional;
+    private Text countryNameAdditional;
     
     @FXML
     private Text rankNumber;
 
     @FXML
-    private Text record;
+    private Text recordWins;
+
+    @FXML
+    private Text recordLosses;
 
     @FXML
     private Text stadiumName;
@@ -29,19 +30,15 @@ public class TeamInfoAdditionalController extends BaseController {
     @FXML
     private Text coachName;
 
-    // @FXML
-    // private TableView roster;  //Future project to display roster in additional info
+    @FXML
+    private TableView roster;
 
     @FXML
-    private ImageView flagImageView;
-
-    private Image flagImage;
+    private ImageView flagImage;
 
     private int wins;
 
     private int losses;
-
-    private int ties;
 
     //INITIALIZE
     @Override
@@ -49,31 +46,30 @@ public class TeamInfoAdditionalController extends BaseController {
 
     //METHODS
 
-    //All methods to update record
-    public void updateRecord() {
-        record.setText(String.valueOf(wins) + " - " + String.valueOf(ties) + " - " + String.valueOf(losses));
+    //2 methods to update records
+    //accumulated wins
+    public void addWin() {
+        wins++;
+        recordWins.setText(Integer.toString(wins));
+    }
+    //accumulated losses
+    public void addLoss() {
+        losses++;
+        recordLosses.setText(Integer.toString(losses));
     }
 
-    public void addWin() {wins++;}
-    public void addLoss() {losses++;}
-    public void addTie() {ties++;}
-
     //GETTERS
-    // public TableView getRoster() {return roster;}
-    public ImageView getFlag() {return flagImageView;}
+    public TableView getRoster() {
+        return roster;
+    }
 
     //SETTERS
     public void setCountryName(String country) {
-        if (country.length() >= 20) {
-            countryNameAdditional.setFont(new Font(15));
-            countryNameAdditional.setStyle("-fx-font-weight: bold;");
-        }
-
         countryNameAdditional.setText(country);
     }
 
     public void setFlagImage(Image flag) {
-        flagImageView.setImage(flag);
+        flagImage.setImage(flag);
     }
 
     public void setRank(String rank) {
@@ -81,9 +77,6 @@ public class TeamInfoAdditionalController extends BaseController {
     }
 
     public void setStadiumName(String name) {
-        if (name.length() >= 30) {
-            stadiumName.setFont(new Font(10));
-        }
         stadiumName.setText(name);
     }
 
@@ -91,7 +84,7 @@ public class TeamInfoAdditionalController extends BaseController {
         coachName.setText(name);
     }
 
-    // public void setRoster(TableView roster) {
-    //     this.roster = roster;
-    // }
+    public void setRoster(TableView roster) {
+        this.roster = roster;
+    }
 }
