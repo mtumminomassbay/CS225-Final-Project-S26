de8277696c9b3fc4724c94c136f107e3b48313ae
Pranshu-2006 <pranshushah2024@gmail.com>
2026-05-07 01:04:06 -0400

    added a button to navigate to Dashboard. TODO: add bracket lines to connect the matchcards

--- src/KnockoutPhaseController.java
+++ src/KnockoutPhaseController.java
@@ -1,12 +1,20 @@
 /*
     Controller for knockout.fxml
 */
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 public class KnockoutPhaseController extends BaseController{
+    private Button Dashboard_button;
 
     @Override
     protected void onLoad() {
 
     }
     
+
+    @FXML
+    private void Dashboard_button_clicked() {
+        navigateTo(Screen.DASHBOARD);
+    }
 }
