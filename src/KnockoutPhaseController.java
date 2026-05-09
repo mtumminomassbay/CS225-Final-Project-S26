05faa50c7e8567099d3a29f99c899f05032fac5e
Pranshu-2006 <pranshushah2024@gmail.com>
2026-05-09 18:41:55 -0400

    Dashboard button now works. Added a scrolling feature. Fixed screen sizing problem. TO-DO: just connect it to the backend and add simulation controls. BUG: whenever I click on any part of the screen, some of the text shrinks. working on fixing this

--- src/KnockoutPhaseController.java
+++ src/KnockoutPhaseController.java
@@ -3,13 +3,19 @@
 */
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class KnockoutPhaseController extends BaseController{
+    @FXML
     private Button Dashboard_button;
+
+    @FXML
+    private ImageView trophyImage;
 
     @Override
     protected void onLoad() {
-
+        trophyImage.setImage(new Image(getClass().getResourceAsStream("/images/trophy.png")));
     }
     
 
