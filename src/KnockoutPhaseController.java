f7f7000b85fcb70dedfdeb1055777c54d7b14c91
Danielle <dmgullage@gmail.com>
2026-05-12 10:17:37 -0400

    Updated knockout GUI

--- src/KnockoutPhaseController.java
+++ src/KnockoutPhaseController.java
@@ -3,15 +3,21 @@
 */
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class KnockoutPhaseController extends BaseController{
+    @FXML
     private Button Dashboard_button;
+
+    @FXML
+    private ImageView trophyImage;
 
     @Override
     protected void onLoad() {
-
+        trophyImage.setImage(new Image(getClass().getResourceAsStream("/images/trophy.png")));
     }
-    
+
 
     @FXML
     private void Dashboard_button_clicked() {
