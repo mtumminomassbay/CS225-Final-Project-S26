731429354f356aec98694eaae6ab946f81a2fb38
jpjcode <jpjoseph8145@gmail.com>
2026-05-12 14:19:29 -0400

    Changed group attribute in Team class resulting in changes in GroupStage.createGroup

--- src/Group.java
+++ src/Group.java
@@ -29,6 +29,7 @@ public class Group {
 
         for (Team team : teams) {
             groupResults.put(team, new GroupResults(team));
+            team.setGroup(this);
         }
 
         createMatches();
@@ -139,5 +140,10 @@ public class Group {
 
     public boolean isCompleted() {
         return completed;
+    }
+
+    @Override
+    public String toString() {
+        return groupName;
     }
 }
